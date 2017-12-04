d04a3971af61c4484effdaf6c1aac1eee0d150bc
Johannes Weiß <johannesweiss@apple.com>
2017-12-04 12:13:14 +0000

    add scaffolding

---
+++ Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version:4.0
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftNIO open source project
+//
+// Copyright (c) 2017-2018 Apple Inc. and the SwiftNIO project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftNIO project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-nio-http2",
+    products: [
+        .executable(name: "NIOHTTP2Server", targets: ["NIOHTTP2Server"]),
+        .library(name: "NIOHTTP2", targets: ["NIOHTTP2"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-nio.git", from: "1.0.0"),
+        .package(url: "git@github.com:apple/swift-nio-nghttp2-support.git", from: "1.0.0"),
+    ],
+    targets: [
+        .target(name: "CNIONghttp2"),
+        .target(name: "NIOHTTP2Server",
+            dependencies: ["NIOHTTP2"]),
+        .target(name: "NIOHTTP2",
+            dependencies: ["NIO", "NIOHTTP1", "CNIONghttp2"]),
+        .testTarget(name: "NIOHTTP2Tests",
+            dependencies: ["NIO", "NIOHTTP1", "NIOHTTP2"]),
+    ]
+)
