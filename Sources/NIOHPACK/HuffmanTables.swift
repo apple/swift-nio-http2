6afb9be363081522f9c4019deb42b588863a3bbf
Jim Dovey <jimdovey@mac.com>
2018-08-02 11:42:32 -0700

    Added missing license headers and updated CONTRIBUTORS.txt via script.

--- Sources/NIOHPACK/HuffmanTables.swift
+++ Sources/NIOHPACK/HuffmanTables.swift
@@ -1,9 +1,16 @@
+//===----------------------------------------------------------------------===//
 //
-//  HuffmanTables.swift
-//  NIOHPACK
+// This source file is part of the SwiftNIO open source project
 //
-//  Created by Jim Dovey on 6/28/18.
+// Copyright (c) 2017-2018 Apple Inc. and the SwiftNIO project authors
+// Licensed under Apache License v2.0
 //
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftNIO project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
 
 typealias HuffmanTableEntry = (bits: UInt32, nbits: Int)
 
