7e17e72f412d14c722e7f189248c761de771e3f4
Johannes Weiss <johannesweiss@apple.com>
2019-03-22 14:05:10 +0000

    update to latest EmbeddedChannel API (#68)
    Motivation:
    
    EmbeddedChannel's API has changed a litle bit, we should update.
    
    Modifications:
    
    update
    
    Result:
    
    code will continue to work

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         .library(name: "NIOHTTP2", targets: ["NIOHTTP2"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0-convergence.1"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0-convergence.4"),
     ],
     targets: [
         .target(name: "NIOHTTP2Server",
