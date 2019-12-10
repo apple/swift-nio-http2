bc1b8ce4eb0b58b2222519cd1c29542c554d505b
Johannes Weiss <johannesweiss@apple.com>
2019-12-10 12:33:12 +0000

    fix new NIO and new Swift compiler warnings

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         .library(name: "NIOHTTP2", targets: ["NIOHTTP2"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.9.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.11.0"),
     ],
     targets: [
         .target(name: "NIOHTTP2Server",
