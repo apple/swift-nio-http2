d3ed8b2034f06aa4e29adb812e9fbafba4ebbd91
George Barnett <gbarnett@apple.com>
2020-03-11 11:34:49 +0000

    Exert backpressure via stream flow control windows
    Motivation:
    
    When a window update event is received in the stream channel we notify
    the inbound window manager. Doing so may emit a WINDOW_UPDATE frame
    to indicate the remote may send us more DATA. However, this happens even
    if there are inbound frames waiting to be delivered to the channel. This
    may lead to the remote sending us more DATA which we have to buffer
    until it is read.
    
    Modifications:
    
    - Update the inbound window manager to track the number of bytes
      buffered in the stream channel
    - Account for buffered bytes when receiving a new window size event
    - Possibly emit WINDOW_UPDATE after unbuffering inbound frames into the
      stream channel's pipeline
    
    Result:
    
    The timing of WINDOW_UPDATE frames is better aligned with when frames
    are actually read in a stream.

--- scripts/sanity.sh
+++ scripts/sanity.sh
@@ -18,7 +18,7 @@ here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 function replace_acceptable_years() {
     # this needs to replace all acceptable forms with 'YEARS'
-    sed -e 's/2017-201[89]/YEARS/g' -e 's/2019/YEARS/g'
+    sed -e 's/2017-201[89]/YEARS/g' -e 's/2019/YEARS/g' -e 's/2020/YEARS/g'
 }
 
 printf "=> Checking linux tests... "
