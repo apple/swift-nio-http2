59e864c1a2af3410c3627cb58be8ac0f97fd1aaf
Peter Adams <63288215+PeterAdams-A@users.noreply.github.com>
2020-07-23 14:08:48 +0100

    Enable thread sanitizer in CI (#212)
    Motivation:
    
    Thread Sanitizer will find bugs which might be missed otherwise.
    
    Modifications:
    
    When running docker image for 1604/5.1 tests and h2spec are both compiled with --sanitize=thread
    
    Result:
    
    Tests are run with thread sanitizer turned on.
    
    Co-authored-by: Johannes Weiss <johannesweiss@apple.com>

--- scripts/test_h2spec.sh
+++ scripts/test_h2spec.sh
@@ -36,8 +36,9 @@ function stop_server() {
 }
 
 # Simple thing to do. Start the server in the background.
-swift build
-"$(swift build --show-bin-path)/NIOHTTP2Server" 127.0.0.1 8888 > /dev/null 2>&1 & disown
+# Allow extra build arguments from the command line - eg tsan.
+swift build $@
+"$(swift build --show-bin-path)/NIOHTTP2Server" 127.0.0.1 8888 > /dev/null & disown
 SERVER_PID=$!
 echo "$SERVER_PID"
 
