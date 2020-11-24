a08dce89a68cc84aa54159340566400af4973050
Cory Benfield <lukasa@apple.com>
2020-11-24 12:57:37 +0000

    Implement StreamMap and replace Dictionary (#258)
    Motivation:
    
    When working with HTTP/2 we frequently have a need to store "per-stream"
    state. This is state that is independently reproduced once per stream.
    We often need to look up this state by stream ID, and so we are
    motivated to store this state as efficiently as possible.
    
    Naively, Dictionary seems like a useful data type for this. However,
    Dictionaries are not free. The cost of hashing in order to index into a
    Dictionary can be substantial. This is both because the hashing function
    itself is branchy and complex, but also because the dictionary access is
    very unfriendly to the branch predictor and the memory caching systems.
    
    We can take advantage of the fact that if you split streams into client
    and server namespaces, stream IDs are strictly ordered. That is, streams
    are created in order of increasing stream ID. While streams are
    _retired_ out of order, at no point will a CircularBuffer of stream IDs
    ever be unordered.
    
    This unlocks a very powerful data structure for us: the ordered "Array"
    (in our case actually CircularBuffer). An ordered Array supports two
    powerful lookup tools. Firstly, it supports the mighty binary search.
    This means the absolute worst-case searching performance for an ordered
    Array is log(n) in the size of the Array. Finding a stream by stream ID
    in an Array of one million streams would take around 20 steps: extremely
    cheap.
    
    For smaller sizes, however, we unlock the one true searching function:
    linear scan. On modern CPUs, performing simple computations (such as
    checking if one number equals another) is orders of magnitude faster
    than looking data up in memory. More importantly, modern CPUs can often
    effectively compute multiple lookups at once thanks to their
    out-of-order superscalar pipelines. As a result, on any modern CPU
    (including all mobile CPUs) a linear search outperforms almost any other
    searching algorithm for surprisingly large Array sizes!
    
    Given that stream IDs are intrinsically sorted, and thus all
    per-stream-data is intrinsically sorted by stream ID, we have the
    ability to use either of the above strategies. More importantly for us,
    we have the ability to be _adaptive_ to those strategies, and swap
    between them as needed.
    
    Thus we deploy the StreamMap. This data structure stores per-stream data
    in a pair of circular buffers, keyed by stream ID. For smallish numbers
    of streams we just search these buffers linearly. For larger numbers, we
    flip over to binary search.
    
    Why circular buffers? Because we avoid a compaction problem. While in
    general removal of stream IDs is done in arbitrary order, in _general_
    the mostly likely streams to end at any given time are the oldest and
    the youngest. Circular buffers make removing streams at those ends very
    cheap, as compaction can be done easily. This, along with avoiding new
    allocations, makes circular buffers a perfect data structure for this
    strategy.
    
    Modifications:
    
    - Implemented StreamMap
    - Replaced Dictionary
    
    Results:
    
    Meaningful performance gains on all benchmarks. 1k requests with 1
    concurrent stream sees about a .1% performance improvement. 1k requests
    with 100 concurrent streams sees a more like 15% performance
    improvement.
    
    Co-authored-by: George Barnett <gbarnett@apple.com>

--- Sources/NIOHTTP2/Frame Buffers/OutboundFlowControlBuffer.swift
+++ Sources/NIOHTTP2/Frame Buffers/OutboundFlowControlBuffer.swift
@@ -16,7 +16,7 @@ import NIO
 /// A structure that manages buffering outbound frames for active streams to ensure that those streams do not violate flow control rules.
 internal struct OutboundFlowControlBuffer {
     /// A buffer of the data sent on the stream that has not yet been passed to the the connection state machine.
-    private var streamDataBuffers: [HTTP2StreamID: StreamFlowControlState]
+    private var streamDataBuffers: StreamMap<StreamFlowControlState>
 
     // TODO(cory): This will eventually need to grow into a priority implementation. For now, it's sufficient to just
     // use a set and worry about the data structure costs later.
@@ -30,10 +30,8 @@ internal struct OutboundFlowControlBuffer {
     internal var maxFrameSize: Int
 
     internal init(initialConnectionWindowSize: Int = 65535, initialMaxFrameSize: Int = 1<<14) {
-        /// By and large there won't be that many concurrent streams floating around, so we pre-allocate a decent-ish
-        /// size.
         // TODO(cory): HEAP! This should be a heap, sorted off the number of pending bytes!
-        self.streamDataBuffers = Dictionary(minimumCapacity: 16)
+        self.streamDataBuffers = StreamMap()
         self.connectionWindowSize = initialConnectionWindowSize
         self.maxFrameSize = initialMaxFrameSize
     }
@@ -45,7 +43,7 @@ internal struct OutboundFlowControlBuffer {
         switch frame.payload {
         case .data:
             // We buffer DATA frames.
-            if !self.streamDataBuffers[frame.streamID].apply({ $0.dataBuffer.bufferWrite((frame.payload, promise)) }) {
+            if !self.streamDataBuffers.apply(streamID: frame.streamID, { $0.dataBuffer.bufferWrite((frame.payload, promise)) }) {
                 // We don't have this stream ID. This is an internal error, but we won't precondition on it as
                 // it can happen due to channel handler misconfiguration or other weirdness. We'll just complain.
                 throw NIOHTTP2Errors.noSuchStream(streamID: frame.streamID)
@@ -55,7 +53,7 @@ internal struct OutboundFlowControlBuffer {
             // Headers are special. If we have a data frame buffered, we buffer behind it to avoid frames
             // being reordered. However, if we don't have a data frame buffered we pass the headers frame on
             // immediately, as there is no risk of violating ordering guarantees.
-            let bufferResult = self.streamDataBuffers[frame.streamID].modify { (state: inout StreamFlowControlState) -> Bool in
+            let bufferResult = self.streamDataBuffers.modify(streamID: frame.streamID) { (state: inout StreamFlowControlState) -> Bool in
                 if state.dataBuffer.haveBufferedDataFrame {
                     state.dataBuffer.bufferWrite((frame.payload, promise))
                     return true
@@ -97,7 +95,7 @@ internal struct OutboundFlowControlBuffer {
     internal mutating func updateWindowOfStream(_ streamID: HTTP2StreamID, newSize: Int32) {
         assert(streamID != .rootStream)
 
-        self.streamDataBuffers[streamID].apply {
+        self.streamDataBuffers.apply(streamID: streamID) {
             let hadData = $0.hasPendingData
             $0.currentWindowSize = Int(newSize)
             if $0.hasPendingData && !hadData {
@@ -111,7 +109,7 @@ internal struct OutboundFlowControlBuffer {
     }
 
     internal func invalidateBuffer(reason: ChannelError) {
-        for buffer in self.streamDataBuffers.values {
+        self.streamDataBuffers.forEachValue { buffer in
             buffer.dataBuffer.failAllWrites(error: reason)
         }
     }
@@ -120,7 +118,7 @@ internal struct OutboundFlowControlBuffer {
         assert(streamID != .rootStream)
 
         let streamState = StreamFlowControlState(streamID: streamID, initialWindowSize: Int(initialWindowSize))
-        self.streamDataBuffers[streamID] = streamState
+        self.streamDataBuffers.insert(streamState)
     }
 
     // We received a stream closed event. Drop any stream state we're holding.
@@ -128,7 +126,7 @@ internal struct OutboundFlowControlBuffer {
     // - returns: Any buffered stream state we may have been holding so their promises can be failed.
     internal mutating func streamClosed(_ streamID: HTTP2StreamID) -> MarkedCircularBuffer<(HTTP2Frame.FramePayload, EventLoopPromise<Void>?)>? {
         self.flushableStreams.remove(streamID)
-        guard var streamData = self.streamDataBuffers.removeValue(forKey: streamID) else {
+        guard var streamData = self.streamDataBuffers.removeValue(forStreamID: streamID) else {
             // Huh, we didn't have any data for this stream. Oh well. That was easy.
             return nil
         }
@@ -148,7 +146,7 @@ internal struct OutboundFlowControlBuffer {
             return nil
         }
 
-        let nextWrite = self.streamDataBuffers[nextStreamID].modify { (state: inout StreamFlowControlState) -> DataBuffer.BufferElement in
+        let nextWrite = self.streamDataBuffers.modify(streamID: nextStreamID) { (state: inout StreamFlowControlState) -> DataBuffer.BufferElement in
             let nextWrite = state.nextWrite(maxSize: min(self.connectionWindowSize, self.maxFrameSize))
             if !state.hasPendingData {
                 self.flushableStreams.remove(nextStreamID)
@@ -197,7 +195,7 @@ extension OutboundFlowControlBuffer {
 }
 
 
-private struct StreamFlowControlState {
+private struct StreamFlowControlState: PerStreamData {
     let streamID: HTTP2StreamID
     var currentWindowSize: Int
     var dataBuffer: DataBuffer
@@ -344,16 +342,7 @@ private extension IOData {
 }
 
 
-private extension Optional where Wrapped == StreamFlowControlState {
-    // This function exists as a performance optimisation: by mutating the optional returned from Dictionary directly
-    // inline, we can avoid the dictionary needing to hash the key twice, which it would have to do if we removed the
-    // value, mutated it, and then re-inserted it.
-    //
-    // However, we need to be a bit careful here, as the performance gain from doing this would be completely swamped
-    // if the Swift compiler failed to inline this method into its caller. This would force the closure to have its
-    // context heap-allocated, and the cost of doing that is vastly higher than the cost of hashing the key a second
-    // time. So for this reason we make it clear to the compiler that this method *must* be inlined at the call-site.
-    // Sorry about doing this!
+extension StreamMap where Element == StreamFlowControlState {
     //
     /// Apply a transform to a wrapped DataBuffer.
     ///
@@ -361,45 +350,8 @@ private extension Optional where Wrapped == StreamFlowControlState {
     ///     - body: A block that will modify the contained value in the
     ///         optional, if there is one present.
     /// - returns: Whether the value was present or not.
-    @inline(__always)
     @discardableResult
-    mutating func apply(_ body: (inout Wrapped) -> Void) -> Bool {
-        if self == nil {
-            return false
-        }
-
-        var unwrapped = self!
-        self = nil
-        body(&unwrapped)
-        self = unwrapped
-        return true
-    }
-
-    // This function exists as a performance optimisation: by mutating the optional returned from Dictionary directly
-    // inline, we can avoid the dictionary needing to hash the key twice, which it would have to do if we removed the
-    // value, mutated it, and then re-inserted it.
-    //
-    // However, we need to be a bit careful here, as the performance gain from doing this would be completely swamped
-    // if the Swift compiler failed to inline this method into its caller. This would force the closure to have its
-    // context heap-allocated, and the cost of doing that is vastly higher than the cost of hashing the key a second
-    // time. So for this reason we make it clear to the compiler that this method *must* be inlined at the call-site.
-    // Sorry about doing this!
-    //
-    /// Apply a transform to a wrapped DataBuffer and return the result.
-    ///
-    /// - parameters:
-    ///     - body: A block that will modify the contained value in the
-    ///         optional, if there is one present.
-    /// - returns: The return value of the modification, or nil if there was no object to modify.
-    mutating func modify<T>(_ body: (inout Wrapped) -> T) -> T? {
-        if self == nil {
-            return nil
-        }
-
-        var unwrapped = self!
-        self = nil
-        let r = body(&unwrapped)
-        self = unwrapped
-        return r
+    fileprivate mutating func apply(streamID: HTTP2StreamID, _ body: (inout Element) -> Void) -> Bool {
+        return self.modify(streamID: streamID, body) != nil
     }
 }
