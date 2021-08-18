42bdcae4ac4913507a5ee7af963c559deb60d1fc
Fabian Fett <fabianfett@apple.com>
2021-08-18 16:22:51 +0200

    Use non-deprecated API for removing handlers (#297)
    ### Motivation:
    
    `removeHandlers(channel:)` was deprecated in NIO 2.32.0.
    
    ### Modifications:
    
    - Raise minimum required `NIO` version to 2.32.0
    - Use `removeHandlers(pipeline:)`
    
    ### Result:
    
    We don't use deprecated API.

--- Sources/NIOHTTP2/HTTP2StreamChannel.swift
+++ Sources/NIOHTTP2/HTTP2StreamChannel.swift
@@ -605,7 +605,7 @@ final class HTTP2StreamChannel: Channel, ChannelCore {
         self.pipeline.fireChannelInactive()
 
         self.eventLoop.execute {
-            self.removeHandlers(channel: self)
+            self.removeHandlers(pipeline: self.pipeline)
             self.closePromise.succeed(())
             if let streamID = self.streamID {
                 self.multiplexer.childChannelClosed(streamID: streamID)
@@ -630,7 +630,7 @@ final class HTTP2StreamChannel: Channel, ChannelCore {
         self.pipeline.fireChannelInactive()
 
         self.eventLoop.execute {
-            self.removeHandlers(channel: self)
+            self.removeHandlers(pipeline: self.pipeline)
             self.closePromise.fail(error)
             if let streamID = self.streamID {
                 self.multiplexer.childChannelClosed(streamID: streamID)
