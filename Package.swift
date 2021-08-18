9389c0b42992929a5dd7218ef56268fbf0355954
Fabian Fett <fabianfett@apple.com>
2021-08-18 15:57:48 +0200

    Use non-deprecated API for removing handlers
    ### Motivation:
    
    removeHandlers(channel:) was deprecated in NIO 2.32.0.
    
    ### Modifications:
    
    - Raise minimum required NIO version to 2.32.0
    - Use removeHandlers(pipeline:)
    
    ### Result:
    
    We don't use deprecated API.

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         .library(name: "NIOHTTP2", targets: ["NIOHTTP2"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.30.0")
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.32.0")
     ],
     targets: [
         .target(
