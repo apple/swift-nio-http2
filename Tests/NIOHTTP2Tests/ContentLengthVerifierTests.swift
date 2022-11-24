15d774cf57f473144f66eb0f9997d8252f5c1e41
Rick Newton-Rogers <rnewtonrogers@apple.com>
2022-11-24 17:25:07 +0000

    add copyright header to ContentLengthVerifierTests

--- Tests/NIOHTTP2Tests/ContentLengthVerifierTests.swift
+++ Tests/NIOHTTP2Tests/ContentLengthVerifierTests.swift
@@ -1,6 +1,20 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftNIO open source project
+//
+// Copyright (c) 2022 Apple Inc. and the SwiftNIO project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftNIO project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import XCTest
 import NIOHPACK
 @testable import NIOHTTP2
-import XCTest
 
 class ContentLengthVerifierTests: XCTestCase {
     func testDuplicatedLengthHeadersPermitted() throws {
