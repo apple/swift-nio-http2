e7bb7d48d2e4c295bce03a0617135c6489c496e8
Aryan Shah <aryan_shah@apple.com>
2024-07-04 14:52:23 +0100

    Fix bug where client incorrectly ignores HEADERS frames on open stream when locally quiescing

--- Sources/NIOHTTP2/ConnectionStateMachine/FrameReceivingStates/ReceivingHeadersState.swift
+++ Sources/NIOHTTP2/ConnectionStateMachine/FrameReceivingStates/ReceivingHeadersState.swift
@@ -61,6 +61,18 @@ extension ReceivingHeadersState {
     }
 }
 
+extension HTTP2ConnectionStateMachine.ConnectionRole {
+    /// Obtain the inverse role to self
+    /// - Returns: The inverse role
+    fileprivate func inverse() -> Self {
+        switch self {
+        case .server:
+            return .client
+        case .client:
+            return .server
+        }
+    }
+}
 
 extension ReceivingHeadersState where Self: LocallyQuiescingState {
     /// Called when we receive a HEADERS frame in this state.
@@ -74,7 +86,10 @@ extension ReceivingHeadersState where Self: LocallyQuiescingState {
         let localSupportsExtendedConnect = self.localSupportsExtendedConnect
         let remoteSupportsExtendedConnect = self.remoteSupportsExtendedConnect
 
-        if streamID.mayBeInitiatedBy(.client) && streamID > self.lastRemoteStreamID {
+        // We are in `LocallyQuiescingState`. The sender of the GOAWAY is `self.role`, so the receiver is the inverse of `self.role`.
+        let receiver = self.role.inverse()
+
+        if streamID.mayBeInitiatedBy(receiver) && streamID > self.lastRemoteStreamID {
             return StateMachineResultWithEffect(result: .ignoreFrame, effect: nil)
         }
 
